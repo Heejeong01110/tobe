c16ee1f253bf8e6d1abe7a6ffb456887c285bd69
Heejeong01110 <123rkdrkd@naver.com>
2022-03-31 15:18:57 +0900

    Initial commit

---
+++ src/main/java/spring/ex/tobe/TobeApplication.java
@@ -0,0 +1,13 @@
+package spring.ex.tobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TobeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/spring/ex/tobe/TobeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ex.tobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
