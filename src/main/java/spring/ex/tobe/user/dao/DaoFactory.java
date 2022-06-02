4fd37c84ca5349c5d1d10de865d9ad6b5b49bfca
Heejeong01110 <123rkdrkd@naver.com>
2022-06-02 14:37:33 +0900

    feat: xml에 db 정보 추가

--- src/main/java/spring/ex/tobe/user/dao/DaoFactory.java
+++ src/main/java/spring/ex/tobe/user/dao/DaoFactory.java
@@ -3,6 +3,7 @@ package spring.ex.tobe.user.dao;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import spring.ex.tobe.user.dao.Conncetion.CustomDataSource;
 
 @Configuration
@@ -24,6 +25,11 @@ public class DaoFactory {
 
   @Bean
   public DataSource dataSource() {
-    return new CustomDataSource().getDataSource();
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+    dataSource.setUrl("jdbc:mysql://localhost/tobe");
+    dataSource.setUsername("spring");
+    dataSource.setPassword("root1234");
+    return dataSource;
   }
 }
