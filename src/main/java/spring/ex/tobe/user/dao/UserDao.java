bfa9265d5c4e579043c00258910979bbd02126fb
Heejeong01110 <123rkdrkd@naver.com>
2022-06-14 15:14:05 +0900

    refactor: DI 주입

--- src/main/java/spring/ex/tobe/user/dao/UserDao.java
+++ src/main/java/spring/ex/tobe/user/dao/UserDao.java
@@ -63,29 +63,8 @@ public class UserDao {
   }
 
   public void deleteAll() throws SQLException {
-    Connection c = null;
-    PreparedStatement ps = null;
-    try {
-      c = dataSource.getConnection();
-      StatementStrategy strategy = new DeleteAllStatement();
-      ps = strategy.makePreparedStatement(c);
-      ps.executeUpdate();
-    } catch (SQLException e) {
-      throw e;
-    } finally {
-      if (ps != null) {
-        try {
-          ps.close();
-        } catch (SQLException e) {
-        }
-      }
-      if (c != null) {
-        try {
-          c.close();
-        } catch (SQLException e) {
-        }
-      }
-    }
+    StatementStrategy strategy = new DeleteAllStatement();
+    jdbcContextWithStatementStrategy(strategy);
   }
 
   public int getCount() throws SQLException {
@@ -118,6 +97,31 @@ public class UserDao {
       if (rs != null) {
         try {
           rs.close();
+        } catch (SQLException e) {
+        }
+      }
+    }
+  }
+
+  public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+    Connection c = null;
+    PreparedStatement ps = null;
+    try {
+      c = dataSource.getConnection();
+      ps = stmt.makePreparedStatement(c);
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      throw e;
+    } finally {
+      if (ps != null) {
+        try {
+          ps.close();
+        } catch (SQLException e) {
+        }
+      }
+      if (c != null) {
+        try {
+          c.close();
         } catch (SQLException e) {
         }
       }
