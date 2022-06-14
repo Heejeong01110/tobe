a7b5e14fa29b570eebf475f243fb505412db3dc6
Heejeong01110 <123rkdrkd@naver.com>
2022-06-14 15:12:09 +0900

    refactor: 전략패턴 구현

---
+++ src/main/java/spring/ex/tobe/user/dao/StatementStrategy.java
@@ -0,0 +1,10 @@
+package spring.ex.tobe.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+
+  PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
