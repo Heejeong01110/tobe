9f69ebe046a9b1a668b786bc6124b3064ad10f18
Heejeong01110 <123rkdrkd@naver.com>
2022-06-15 17:40:42 +0900

    refactor: 콜백 구조 단순화

--- src/main/java/spring/ex/tobe/learningtest/template/Calculator.java
+++ src/main/java/spring/ex/tobe/learningtest/template/Calculator.java
@@ -26,27 +26,38 @@ public class Calculator {
     }
   }
 
-  public int calcSum(String path) throws IOException {
-    BufferedReaderCallback callback = br -> {
-      Integer sum = 0;
+  public int lineReadTemplate(String path, LineCallback callback, int initVal)
+      throws IOException {
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new FileReader(path));
+      int res = initVal;
       String line = null;
       while ((line = br.readLine()) != null) {
-        sum += Integer.parseInt(line);
+        res = callback.doSomethingWithLine(line, res);
       }
-      return sum;
-    };
-    return fileReadTemplate(path, callback);
+      return res;
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      throw e;
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          System.out.println(e.getMessage());
+        }
+      }
+    }
+  }
+
+  public int calcSum(String path) throws IOException {
+    LineCallback callback = (line, value) -> value + Integer.parseInt(line);
+    return lineReadTemplate(path, callback, 0);
   }
 
   public int calcMultiply(String path) throws IOException {
-    BufferedReaderCallback callback = br -> {
-      Integer sum = 1;
-      String line = null;
-      while ((line = br.readLine()) != null) {
-        sum *= Integer.parseInt(line);
-      }
-      return sum;
-    };
-    return fileReadTemplate(path, callback);
+    LineCallback callback = (line, value) -> value * Integer.parseInt(line);
+    return lineReadTemplate(path, callback, 1);
   }
 }
---
+++ src/main/java/spring/ex/tobe/learningtest/template/LineCallback.java
@@ -0,0 +1,6 @@
+package spring.ex.tobe.learningtest.template;
+
+public interface LineCallback {
+
+  Integer doSomethingWithLine(String Line, Integer value);
+}
