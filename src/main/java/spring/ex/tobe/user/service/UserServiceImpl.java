cae2662340607638a69cccf4d3157a658380a7fa
Heejeong01110 <123rkdrkd@naver.com>
2022-08-02 02:26:29 +0900

    refactor: 스프링 제공 인터페이스로 변경

--- src/main/java/spring/ex/tobe/user/service/UserServiceImpl.java
+++ src/main/java/spring/ex/tobe/user/service/UserServiceImpl.java
@@ -1,14 +1,8 @@
 package spring.ex.tobe.user.service;
 
 import java.util.List;
-import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -21,6 +15,7 @@ public class UserServiceImpl implements UserService {
   private UserDao userDao;
   private PlatformTransactionManager transactionManager;
   private UserLevelUpgradePolicy userLevelUpgradePolicy;
+  private MailSender mailSender;
 
   public void setUserDao(UserDao userDao) {
     this.userDao = userDao;
@@ -32,6 +27,10 @@ public class UserServiceImpl implements UserService {
 
   public void setUserLevelUpgradePolicy(UserLevelUpgradePolicy userLevelUpgradePolicy) {
     this.userLevelUpgradePolicy = userLevelUpgradePolicy;
+  }
+
+  public void setMailSender(MailSender mailSender) {
+    this.mailSender = mailSender;
   }
 
   public void upgradeLevels() {
@@ -60,24 +59,13 @@ public class UserServiceImpl implements UserService {
   }
 
   private void sendUpgradeEmail(User user) {
-    Properties props = new Properties();
-    props.put("mail.stmp.host", "mail.ksug.org");
-    Session s = Session.getInstance(props, null);
+    SimpleMailMessage mailMessage = new SimpleMailMessage();
+    mailMessage.setTo(user.getEmail());
+    mailMessage.setFrom("admin@test.com");
+    mailMessage.setSubject("Upgrade 안내");
+    mailMessage.setText("사용자님의 등급이 "+user.getLevel().name()+"로 업그레이드 되었습니다.");
 
-    MimeMessage message = new MimeMessage(s);
-    try{
-      message.setFrom(new InternetAddress("admin@test.com"));
-      message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
-      message.setSubject("Upgrade 안내");
-      message.setText("사용자님의 등급이 "+user.getLevel().name()+"로 업그레이드 되었습니다.");
-
-      Transport.send(message);
-    } catch (AddressException e) {
-      throw new RuntimeException(e);
-    } catch (MessagingException e) {
-      throw new RuntimeException(e);
-    }
-
+    mailSender.send(mailMessage);
   }
 
   public void add(User user) {
