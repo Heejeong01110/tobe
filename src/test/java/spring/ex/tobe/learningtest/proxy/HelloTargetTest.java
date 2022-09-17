97f3ffe215e66f49025ac959ad93bdba554d84fe
Heejeong01110 <123rkdrkd@naver.com>
2022-09-17 18:45:26 +0900

    test: advisor 등록

--- src/test/java/spring/ex/tobe/learningtest/proxy/HelloTargetTest.java
+++ src/test/java/spring/ex/tobe/learningtest/proxy/HelloTargetTest.java
@@ -4,7 +4,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.lang.reflect.Proxy;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 class HelloTargetTest {
 
@@ -23,4 +28,40 @@ class HelloTargetTest {
     assertThat(proxyHello.sayThankYou("Toby"), is("THANKYOU TOBY"));
   }
 
+  @Test
+  public void proxyFactoryBean() {
+    ProxyFactoryBean pfBean = new ProxyFactoryBean();
+    pfBean.setTarget(new HelloTarget());
+    pfBean.addAdvice(new UppercaseAdvice());
+
+    Hello proxyHello = (Hello) pfBean.getObject();
+    assertThat(proxyHello.sayHello("Toby"), is("HELLO TOBY"));
+    assertThat(proxyHello.sayHi("Toby"), is("HI TOBY"));
+    assertThat(proxyHello.sayThankYou("Toby"), is("THANKYOU TOBY"));
+  }
+
+  @Test
+  public void pointcutAdvisor() {
+    ProxyFactoryBean pfBean = new ProxyFactoryBean();
+    pfBean.setTarget(new HelloTarget());
+
+    NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+    pointcut.setMappedName("sayH*");
+    pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+    //advisor : advice와 포인트컷을 둘다 가지고 있는 개념
+
+    Hello proxyHello = (Hello) pfBean.getObject();
+    assertThat(proxyHello.sayHello("Toby"), is("HELLO TOBY"));
+    assertThat(proxyHello.sayHi("Toby"), is("HI TOBY"));
+    assertThat(proxyHello.sayThankYou("Toby"), is("ThankYou Toby"));
+  }
+
+  private static class UppercaseAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+      String ret = (String) invocation.proceed();
+      return ret.toUpperCase();
+    }
+  }
 }
