38071301e4fd2da35d02240b5da20646daaa2e8c
Heejeong01110 <123rkdrkd@naver.com>
2022-09-21 15:27:55 +0900

    feat: sql문 분리

--- src/main/java/spring/ex/tobe/user/dao/UserDaoJdbc.java
+++ src/main/java/spring/ex/tobe/user/dao/UserDaoJdbc.java
@@ -9,7 +9,15 @@ import spring.ex.tobe.user.domain.User;
 
 public class UserDaoJdbc implements UserDao {
 
+  private String sqlAdd;
+  private String sqlGet;
+  private String sqlGetAll;
+  private String sqlDeleteAll;
+  private String sqlGetCount;
+  private String sqlUpdate;
+
   private JdbcTemplate jdbcTemplate;
+
   private RowMapper<User> userMapper = (rs, rowNum) -> {
     User user = new User();
     user.setId(rs.getString("id"));
@@ -29,36 +37,58 @@ public class UserDaoJdbc implements UserDao {
     this.jdbcTemplate = new JdbcTemplate(dataSource);
   }
 
+  public void setSqlAdd(String sqlAdd) {
+    this.sqlAdd = sqlAdd;
+  }
+
+  public void setSqlGet(String sqlGet) {
+    this.sqlGet = sqlGet;
+  }
+
+  public void setSqlGetAll(String sqlGetAll) {
+    this.sqlGetAll = sqlGetAll;
+  }
+
+  public void setSqlDeleteAll(String sqlDeleteAll) {
+    this.sqlDeleteAll = sqlDeleteAll;
+  }
+
+  public void setSqlGetCount(String sqlGetCount) {
+    this.sqlGetCount = sqlGetCount;
+  }
+
+  public void setSqlUpdate(String sqlUpdate) {
+    this.sqlUpdate = sqlUpdate;
+  }
+
   public void add(final User user) {
     jdbcTemplate.update(
-        "insert into users (id, name, password, level, login, recommend, email) values (?, ?, ?, ?, ?, ?, ?)",
+        sqlAdd,
         user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(),
         user.getLogin(), user.getRecommend(), user.getEmail());
   }
 
   public User get(String id) {
-    return jdbcTemplate.queryForObject("select * from users where id = ?",
+    return jdbcTemplate.queryForObject(sqlGet,
         userMapper, id);
   }
 
   public List<User> getAll() {
-    return jdbcTemplate.query("select * from users order by id", userMapper);
+    return jdbcTemplate.query(sqlGetAll, userMapper);
   }
 
   public void deleteAll() {
-    jdbcTemplate.update("delete from users");
+    jdbcTemplate.update(sqlDeleteAll);
   }
 
   public int getCount() {
-    return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    return jdbcTemplate.queryForObject(sqlGetCount, Integer.class);
   }
 
   @Override
   public void update(User user) {
-    jdbcTemplate.update(
-        "update users set name = ?, password = ?, level = ?, login = ?, recommend = ? , email = ? "
-            + "where id = ?", user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(),
-        user.getRecommend(),user.getEmail(), user.getId());
+    jdbcTemplate.update(sqlUpdate, user.getName(), user.getPassword(), user.getLevel().intValue(),
+        user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
   }
 
 }
